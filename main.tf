375906138f7f979350493816ce2a21a2f043967b
Dima <velychkodmytro@gmail.com>
2024-04-16 12:42:05 +0300

    fix: password connectrion

--- main.tf
+++ main.tf
@@ -69,3 +69,10 @@ resource "azurerm_linux_virtual_machine" "this" {
     version   = var.source_image_reference.version
   }
 }
+
+resource "azurerm_network_interface_application_security_group_association" "this" {
+  count = length(var.application_security_group_id) != null ? 1 : 0
+
+  network_interface_id          = azurerm_network_interface.this.id
+  application_security_group_id = var.application_security_group_id
+}
